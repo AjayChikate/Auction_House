b29cf3f990f51a6342df701df4e3036c2211cb0a
Ajay Chikate <ajaychikate55555@gmail.com>
2026-07-17 17:27:23 +0530

    Wrap App component with AuthProvider

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,14 +1,16 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 import App from './App'
-import './index.css';
-
+import { AuthProvider } from './context/AuthContext'
+import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <AuthProvider>
+        <App />
+      </AuthProvider>
     </BrowserRouter>
   </React.StrictMode>
 )
